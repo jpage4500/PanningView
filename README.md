3f084346b36d8d326482965b27922ecaf967523b
Faizaan <aulisius7@gmail.com>
2015-12-14 20:32:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+PanningView
+==================
+
+PanningView is a library which implements the animated background in the now playing screen of the Play Music app.
+
+Planned -> Sample APK either at Releases or Google Play Store.
+
+Including in your project
+-------------------------
+
+Add the library to your application as a library project.
+
+Planned -> the ability to add as Gradle module.
+
+License
+-----------
+
+    Copyright 2013 Flavien Laurent
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+  
---
+++ library/src/main/java/com/maelstrom/panning/lib/PanningImageView.java
@@ -0,0 +1,91 @@
+package com.maelstrom.panning.lib;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class PanningImageView extends ImageView {
+
+	private final PanningImageViewAttacher mAttacher;
+
+	private int mPanningDurationInMs;
+
+	public PanningImageView (Context context) {
+		this (context, null);
+	}
+
+	public PanningImageView (Context context, AttributeSet attr) {
+		this (context, attr, 0);
+	}
+
+	public PanningImageView (Context context, AttributeSet attr, int defStyle) {
+		super (context, attr, defStyle);
+		readStyleParameters (context, attr);
+		super.setScaleType (ScaleType.MATRIX);
+		mAttacher = new PanningImageViewAttacher (this, mPanningDurationInMs);
+	}
+
+
+	private void readStyleParameters(Context context, AttributeSet attributeSet) {
+		TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.PanningImageView);
+		try {
+			mPanningDurationInMs = typedArray.getInt (R.styleable.PanningImageView_panningDurationInMs, PanningImageViewAttacher.DEFAULT_PANNING_DURATION_IN_MS);
+		} finally {
+			typedArray.recycle ();
+		}
+	}
+
+	// setImageBitmap calls through to this method
+	@Override
+	public void setImageDrawable (Drawable drawable) {
+		super.setImageDrawable (drawable);
+		stopUpdateStartIfNecessary ();
+	}
+
+	@Override
+	public void setImageResource (int resId) {
+		super.setImageResource (resId);
+		stopUpdateStartIfNecessary ();
+	}
+
+	@Override
+	public void setImageURI (Uri uri) {
+		super.setImageURI (uri);
+		stopUpdateStartIfNecessary ();
+	}
+
+	/**
+	 *
+	 */
+	private void stopUpdateStartIfNecessary () {
+		if (mAttacher != null) {
+			boolean wasPanning = mAttacher.isPanning();
+			mAttacher.stopPanning ();
+			mAttacher.update ();
+			if(wasPanning) mAttacher.startPanning ();
+		}
+	}
+
+	@Override
+	public void setScaleType (ScaleType scaleType) {
+		throw new UnsupportedOperationException ("Only ScaleType.MATRIX is supported");
+	}
+
+
+	@Override
+	protected void onDetachedFromWindow () {
+		mAttacher.cleanup ();
+		super.onDetachedFromWindow ();
+	}
+
+	public void startPanning () {
+		mAttacher.startPanning ();
+	}
+
+	public void stopPanning () {
+		mAttacher.stopPanning ();
+	}
+}
